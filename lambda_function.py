3e30be7ab53bc014fb060a01a2ecf31d9eed14dc
myeongseok <myeongseok@kaist.ac.kr>
2025-03-18 14:23:56 -0400

    add buildspec yaml file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,5 @@
 def lambda_handler(event, context):
     print("여기서 시작")
-    print("Jane")
-    print("again")
     return {
         'statusCode': 200,
         'body': 'Hello from my toy Lambda function!'
