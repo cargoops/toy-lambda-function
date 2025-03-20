cb1b8ad87e17d92dc396cc3ccbe035e69e17bbbf
myeongseok <myeongseok@kaist.ac.kr>
2025-03-20 17:44:15 -0400

    add requirements.txt

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,17 @@
+import pandas as pd
+
 def lambda_handler(event, context):
-    print("여기서 시작")
+    # 예시: 간단한 DataFrame 생성 후 CSV로 변환하여 반환
+    data = {
+        'Name': ['John', 'Anna', 'Peter'],
+        'Age': [29, 23, 35]
+    }
+    df = pd.DataFrame(data)
+
+    # 확인용 로그
+    print(df)
+
     return {
         'statusCode': 200,
-        'body': '성공!'
+        'body': df.to_csv(index=False)
     }
