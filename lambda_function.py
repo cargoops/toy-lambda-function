015f5f6ca0a8cd85a7f70979fcd16157c1c350e2
myeongseok <myeongseok@kaist.ac.kr>
2025-03-25 18:17:14 -0400

    remove pandas

--- lambda_function.py
+++ lambda_function.py
@@ -2,19 +2,8 @@ import random
 
 def lambda_handler(event, context):
     # 랜덤 이름 선택
-    names = ['Mel', 'Michelle', 'Jane', 'Hyu', 'Edgar']
-    name = random.choice(names)
-
-    # prefix 추가
-    if name in ['Mel', 'Michelle', 'Jane']:
-        name = f'Beautiful {name}'
-    else:
-        name = f'Handsome {name}'
-
-    # 확인용 로그
-    print(name)
 
     return {
         'statusCode': 200,
-        'body': name
+        'body': "hello"
     }
