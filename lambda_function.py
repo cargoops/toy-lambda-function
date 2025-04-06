c55e77ec215f9ebf216b496137b3b90b84de4112
myeongseok <myeongseok@kaist.ac.kr>
2025-04-06 13:17:26 -0400

    code formatting

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,7 @@
 import random
 
+
 def lambda_handler(event, context):
     # 랜덤 이름 선택
 
-    return {
-        'statusCode': 200,
-        'body': "hello"
-    }
+    return {"statusCode": 200, "body": "hello"}
